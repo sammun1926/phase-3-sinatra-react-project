7f851a153b3d6a880ffd7e71481d20abc3ce8f3e
sammun1926 <munorusam10@gmail.com>
2023-06-07 10:35:36 +0300

    added a grade table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_07_071914) do
+ActiveRecord::Schema.define(version: 2023_06_07_073125) do
+
+  create_table "grades", force: :cascade do |t|
+    t.integer "student_id"
+    t.integer "subject_id"
+    t.float "value"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "students", force: :cascade do |t|
     t.string "Firstname"
