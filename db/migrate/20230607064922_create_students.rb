b6b155129c847fb1bbdc72339d4cd49c8088e583
sammun1926 <munorusam10@gmail.com>
2023-06-07 10:12:04 +0300

    added students table

--- db/migrate/20230607064922_create_students.rb
+++ db/migrate/20230607064922_create_students.rb
@@ -1,4 +1,12 @@
 class CreateStudents < ActiveRecord::Migration[6.1]
   def change
+    create_table :students do |t|
+      t.string :Firstname
+      t.string :Lastname
+      t.integer :grade_level
+
+      # Add more attributes as needed
+      t.timestamps null: false
+    end
   end
 end
